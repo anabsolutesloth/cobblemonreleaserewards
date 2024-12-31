63002b5362cb8f4c58dd325e872f5c2be002eb15
anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>
2024-12-30 17:03:40 -0800

    Reworked LevelRangeCondition into PokemonValuerangeCondition Condenses the 'level_range', 'friendship_range', and 'stat_range' into one. specify subtype with 'value_type'. '_stat', '_ev', and '_iv' subtypes are available for every stat.
    Signed-off-by: anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>

--- src/main/java/com/emperdog/releaserewards/loot/conditions/ModLootConditions.java
+++ src/main/java/com/emperdog/releaserewards/loot/conditions/ModLootConditions.java
@@ -11,8 +11,8 @@ public class ModLootConditions {
     public static final DeferredRegister<LootItemConditionType> LOOT_CONDITION_TYPES =
             DeferredRegister.create(Registries.LOOT_CONDITION_TYPE, ReleaseRewards.MODID);
 
-    public static final Supplier<LootItemConditionType> LEVEL_RANGE =
-            LOOT_CONDITION_TYPES.register("level_range", () -> new LootItemConditionType(LevelRangeCondition.CODEC));
+    public static final Supplier<LootItemConditionType> POKEMON_VALUE_RANGE =
+            LOOT_CONDITION_TYPES.register("value_range", () -> new LootItemConditionType(PokemonValueRangeCondition.CODEC));
 
     public static final Supplier<LootItemConditionType> POKEMON_LABEL =
             LOOT_CONDITION_TYPES.register("pokemon_label", () -> new LootItemConditionType(PokemonLabelCondition.CODEC));
---
+++ src/main/java/com/emperdog/releaserewards/loot/conditions/PokemonValueRangeCondition.java
@@ -0,0 +1,94 @@
+package com.emperdog.releaserewards.loot.conditions;
+
+import com.cobblemon.mod.common.api.pokemon.stats.Stats;
+import com.cobblemon.mod.common.pokemon.Pokemon;
+import com.emperdog.releaserewards.ReleaseRewards;
+import com.emperdog.releaserewards.loot.ModLootContextParams;
+import com.google.common.collect.ImmutableSet;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParam;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.LootItemConditionType;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+
+public record PokemonValueRangeCondition(int min, int max, String valueType) implements LootItemCondition {
+
+    public static final MapCodec<PokemonValueRangeCondition> CODEC = RecordCodecBuilder.mapCodec(inst -> inst.group(
+            Codec.INT.fieldOf("min").fieldOf("range").forGetter(PokemonValueRangeCondition::min),
+            Codec.INT.fieldOf("max").fieldOf("range").forGetter(PokemonValueRangeCondition::max),
+            Codec.STRING.fieldOf("value_type").forGetter(PokemonValueRangeCondition::valueType)
+    ).apply(inst, PokemonValueRangeCondition::new));
+
+    @Override
+    public @NotNull LootItemConditionType getType() {
+        return ModLootConditions.POKEMON_VALUE_RANGE.get();
+    }
+
+    @Override
+    public boolean test(@NotNull LootContext context) {
+        Pokemon pokemon = context.getParam(ModLootContextParams.POKEMON);
+        double value = getValue(pokemon, valueType);
+        return value >= min && value <= max;
+    }
+
+    @Override
+    public @NotNull Set<LootContextParam<?>> getReferencedContextParams() {
+        return ImmutableSet.of(ModLootContextParams.POKEMON);
+    }
+
+    public double getValue(Pokemon pokemon, String valueType) {
+        return switch (valueType) {
+            case "level" -> pokemon.getLevel(); // Level
+            case "friendship" -> pokemon.getFriendship(); // Friendship
+            // raw Stats
+            case "hp_stat", "health_stat", "maxhp_stat", "max_hp_stat", "maxhealth_stat", "max_health_stat" ->
+                    pokemon.getMaxHealth();
+            case "atk_stat", "attack_stat" ->
+                    pokemon.getAttack();
+            case "stat_def", "stat_defence", "stat_defense" ->
+                    pokemon.getDefence();
+            case "stat_spa", "stat_spatk", "stat_specialattack", "stat_special_attack" ->
+                    pokemon.getSpecialAttack();
+            case "stat_spd", "stat_spdef", "stat_specialdefence", "stat_specialdefense", "stat_special_defence",
+                 "stat_special_defense" ->
+                    pokemon.getSpecialDefence();
+            case "stat_spe", "stat_speed" ->
+                    pokemon.getSpeed();
+            // raw EVs
+            case "hp_ev", "health_ev" ->
+                    pokemon.getEvs().get(Stats.HP);
+            case "atk_ev", "attack_ev" ->
+                    pokemon.getEvs().get(Stats.ATTACK);
+            case "def_ev", "defence_ev", "defense_ev" ->
+                    pokemon.getEvs().get(Stats.DEFENCE);
+            case "spa_ev", "spatk_ev", "specialattack_ev", "special_attack_ev" ->
+                    pokemon.getEvs().get(Stats.SPECIAL_ATTACK);
+            case "spd_ev", "spdef_ev", "specialdefence_ev", "specialdefense_ev", "special_defence_ev", "special_defense_ev" ->
+                    pokemon.getEvs().get(Stats.SPECIAL_DEFENCE);
+            case "spe_ev", "speed_ev" ->
+                    pokemon.getEvs().get(Stats.SPEED);
+            // raw IVs
+            case "hp_iv", "health_iv" ->
+                    pokemon.getIvs().get(Stats.HP);
+            case "atk_iv", "attack_iv" ->
+                    pokemon.getIvs().get(Stats.ATTACK);
+            case "def_iv", "defence_iv", "defense_iv" ->
+                    pokemon.getIvs().get(Stats.DEFENCE);
+            case "spa_iv", "spatk_iv", "specialattack_iv", "special_attack_iv" ->
+                    pokemon.getIvs().get(Stats.SPECIAL_ATTACK);
+            case "spd_iv", "spdef_iv", "specialdefence_iv", "specialdefense_iv", "special_defence_iv", "special_defense_iv" ->
+                    pokemon.getIvs().get(Stats.SPECIAL_DEFENCE);
+            case "spe_iv", "speed_iv" ->
+                    pokemon.getIvs().get(Stats.SPEED);
+            default -> {
+                ReleaseRewards.LOGGER.warn("pokemon_value type '{}' is invalid! Returning 0.", valueType);
+                yield 0.0;
+            }
+        };
+    }
+}
